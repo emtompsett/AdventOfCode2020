f3585665088fb90dcb7153c488c0ec432740aebf
Emily Tompsett <etompsett@mka.org>
2020-12-01 14:59:22 -0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+#advent of code 2020
+#day 1
+
+def getNums(inNums):
+    for i in range(len(inNums)):
+        for j in range(i+1, len(inNums)):
+            for k in range(j+1, len(inNums)):
+                if int(inNums[i]) + int(inNums[j]) + int(inNums[k]) == 2020:
+                    return int(inNums[i]),int(inNums[j]), int(inNums[k])
+    return false;
+
+def main():
+    file = open("inputDay1.txt", "r");
+    lines = file.read().strip().split();
+    print(lines)
+    nums = getNums(lines)
+    print(nums[0] * nums[1]*nums[2]);
+        
+main();
